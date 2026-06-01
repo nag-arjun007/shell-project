c735e4bab848ea1b115b2867ddd1d782914ff3de
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-01 14:02:29 +0530

    hello world

---
+++ 09-install-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USER=$(id -u)
+
+if [ $USER -ne 0 ]; then
+echo "please run the script with root user"
+exit 1
+fi
+
+#echo "I am continuing"
+
+echo "Installing mysql"
+
+dnf install mysql -y
+if [ $? -eq 0 ]; then
+echo "mysql is install success"
+else
+echo "mysql is not installed"
+exit 1
+fi
