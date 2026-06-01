96ebaf4d7ab68b6bd5ae8dd4dddc578f08ddc4b0
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-01 18:20:19 +0530

    hello world

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-for number in {1..100}
-do
-  echo "$number"
-done  
+USER=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+if [ $USER -ne 0 ]; then
+    echo "please run the script with root user"
+    exit 1
+fi
+
+validate(){
+    if [ $2 -eq 0 ]; then
+       echo "$1 is install success" | tee -a $LOGS_FILE
+    else
+       echo "$1 is not installed .. failure" | tee -a $LOGS_FILE
+       exit 1
+    fi
+}
+
+#echo "I am continuing"
+
+for package in $@
+do   
+    echo "Installing $package"
+done    
