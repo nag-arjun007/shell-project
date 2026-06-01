a0c38c7e0cfde58cb5eda0e3e9696d2e580e04bf
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-01 17:13:07 +0530

    hello world

--- 09-install-functions.sh
+++ 09-install-functions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 USER=$(id -u)
+LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log"
 
 if [ $USER -ne 0 ]; then
     echo "please run the script with root user"
@@ -10,28 +12,28 @@ validate(){
     if [ $2 -eq 0 ]; then
        echo "$1 is install success"
     else
-       echo "$1 is not installed"
+       echo "$1 is not installed .. failure"
        exit 1
     fi
 }
 
 #echo "I am continuing"
-dnf list installed mysql
+dnf list installed mysql &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
     echo "already installed mysql so ... skipping"
 else
     echo "Installing mysql"
-    dnf install mysql -y
+    dnf install mysql -y &>> $LOGS_FILE
     validate mysql $?
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
     echo "nginx is already installed ... SKIPPING"
 else
     echo "Installing nginx"
-    dnf install nginx -y
+    dnf install nginx -y &>> $LOGS_FILE
     validate nginx $?
 fi
