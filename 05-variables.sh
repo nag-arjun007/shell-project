ba8629880eacac1cfde76788a995266d4ac1fc92
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-01 13:21:25 +0530

    hello world

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,3 +1,12 @@
 #!/bin.bash
-TIMESTAMP=$(date)
-echo "time is: $TIMESTAMP"
+#TIMESTAMP=$(date)
+#echo "time is: $TIMESTAMP"
+START_TIME=$(date +%s)
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script executed in $TOTAL_TIME seconds"
