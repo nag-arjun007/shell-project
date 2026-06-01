df43036a75c82df00fd4ad1e1d50df4057792520
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-01 16:15:42 +0530

    hello world

--- 09-install-functions.sh
+++ 09-install-functions.sh
@@ -2,26 +2,36 @@
 USER=$(id -u)
 
 if [ $USER -ne 0 ]; then
-echo "please run the script with root user"
-exit 1
+    echo "please run the script with root user"
+    exit 1
 fi
 
-validate() {
-if [ $2 -eq 0 ]; then
-echo "mysql is install success"
-else
-echo "mysql is not installed"
-exit 1
-fi
+validate(){
+    if [ $2 -eq 0 ]; then
+       echo "$1 is install success"
+    else
+       echo "$1 is not installed"
+       exit 1
+    fi
 }
 
 #echo "I am continuing"
 dnf list installed mysql
-if [ $? -eq 0 ]; then
-echo "already installed mysql so ... skipping"
-else
-echo "Installing mysql"
 
-dnf install mysql -y
-validate mysql $?
+if [ $? -eq 0 ]; then
+    echo "already installed mysql so ... skipping"
+else
+    echo "Installing mysql"
+    dnf install mysql -y
+    validate mysql $?
+fi
+
+dnf list installed nginx
+
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING"
+else
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $?
 fi
