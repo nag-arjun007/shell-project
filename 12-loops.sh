0e745e667ff7cbdf53763b20bef51f3fbb501930
nag-arjun007 <arjunbharat88@gmail.com>
2026-06-02 08:40:35 +0530

    hello world

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,22 +3,17 @@ USER=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-B="\e[34m"
-N="\e[0m"
 
 if [ $USER -ne 0 ]; then
-    echo -e "$Y please run the script with root user $N"
+    echo "please run the script with root user"
     exit 1
 fi
 
 validate(){
     if [ $2 -eq 0 ]; then
-       echo -e "$TIMESTAMP [INFO] $1 is install $G success $N" | tee -a $LOGS_FILE
+       echo "$TIMESTAMP [INFO] $1 is install success" | tee -a $LOGS_FILE
     else
-       echo -e "$TIMESTAMP [ERROR] $1 is not installed ..$Y failure $N" | tee -a $LOGS_FILE
+       echo "$TIMESTAMP [ERROR] $1 is not installed .. failure" | tee -a $LOGS_FILE
        exit 1
     fi
 }
@@ -33,6 +28,6 @@ do
         dnf install $package -y &>> $LOGS_FILE
         validate "installing $package" $?
     else
-        echo -e "$TIMESTAMP [INFO] $package already installed ... $B skipping $N"
+        echo "$TIMESTAMP [INFO] $package already installed ... skipping"
     fi         
 done    
